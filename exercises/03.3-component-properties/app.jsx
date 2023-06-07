a5f41e09f0acdead6c86944ffb40989689a54c42
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-06-07 19:15:52 +0000

    react tutorial exercises

--- exercises/03.3-component-properties/app.jsx
+++ exercises/03.3-component-properties/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropType from "prop-types";
 
-const BootstrapCard = props => {
+const BootstrapCard = (props) => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
@@ -11,7 +11,7 @@ const BootstrapCard = props => {
 				<h5 className="card-title">{props.title}</h5>
 				<p className="card-text">{props.description}</p>
 				<a href={props.buttonUrl} className="btn btn-primary">
-				{props.buttonLabel}
+					{props.buttonLabel}
 				</a>
 			</div>
 		</div>
@@ -19,13 +19,20 @@ const BootstrapCard = props => {
 };
 BootstrapCard.propTypes = {
 	title: PropType.string,
-		// 2) add here the new properties into the proptypes object
 	imageUrl: PropType.string,
 	description: PropType.string,
 	buttonUrl: PropType.string,
-	buttonLabel: PropType.string
+	buttonLabel: PropType.string,
 };
 
-
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
-ReactDOM.render(<BootstrapCard title='Bob Dylan' imageUrl='https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true' description='Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter.' buttonUrl='https://en.wikipedia.org/wiki/Bob_Dylan' buttonLabel='Go to wikipedia' />, document.querySelector("#myDiv"));
+ReactDOM.render(
+	<BootstrapCard
+		title="Bob Dylan"
+		imageUrl="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+		buttonLabel="Go to wikipedia"
+	/>,
+	document.querySelector("#myDiv")
+);
